5a0a637a7589fd7f9ad59b4b96aa68f9f33c2535
ckipp01 <ckipp@pm.me>
2019-04-26 07:03:18 +0200

    add in crate builder information

--- data/crate.js
+++ data/crate.js
@@ -174,6 +174,24 @@ const CRATE = {
       }
     ]
   },
+  'crate-builder': {
+    title: 'crate-builder',
+    template: 'main',
+    body: [
+      {
+        type: 'p',
+        text: 'One of the things I actually dislike about <a href="#gyul">gyul</a> is that all of my data is stored as JS objects. While this makes it extremely easy to parse and deal with, it sort of makes it a nightmare to manage. Writing new pages and managing html layout with it is quite clunky.'
+      },
+      {
+        type: 'p',
+        text: `The way I'd like to get around that is to make some sort of JS object editor. Specifically, in gyul I use a crate.js file which holds all of the entries you're looking at. I'd like to be able to just open that up in a browser and have a more visual way to edit it, add elements to it, and easily read it.`
+      },
+      {
+        type: 'p',
+        text: `I'm currently still exploring the best way to do this, so it may be a while before something working shows up here.`
+      }
+    ]
+  },
   gyul: {
     title: '귤 gyul',
     template: 'main',
